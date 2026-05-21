28dad59601efa32fe88300997256d9c8a57d1dc5
AhmadMrupdate <ahmadibrahimnbb@gmail.com>
2026-05-21 09:08:35 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected!");
   } catch (err) {
     console.error("MongoDB error:", err.message);
-    process.exit(1);
+    // Ba ya tsaya — yana ci gaba
   }
 };
 
